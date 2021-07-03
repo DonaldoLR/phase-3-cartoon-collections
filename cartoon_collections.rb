a7b7a539d3b5e1d66d5f0da8d5a7061a2c22d7b1
github username <github email address>
2021-07-03 06:58:57 -0400

    practice using array methods in ruby

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves_array)# code an argument here
   # Your code here
+  dwarves_array.each_with_index do |dwarf, idx|
+    puts "#{idx + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_array)# code an argument here
   # Your code here
+  planeteer_array.collect do |call|
+    "#{call.capitalize()}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls_array)# code an argument here
   # Your code here
+  calls_array.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array_of_strings)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  array_of_strings.detect do |string|
+    cheese_types.include?(string)
+  end
 end
